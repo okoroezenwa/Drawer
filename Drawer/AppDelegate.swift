fb4f9c0b15c95d1eb3c3969a1be77c1c018bd818
Ezenwa Okoro <okoroezenwa@bitbucket.org>
2021-02-04 18:36:41 +0100

    - Updated view placements - Defaulted to 3D transforms on iOS 14

--- Drawer/AppDelegate.swift
+++ Drawer/AppDelegate.swift
@@ -15,7 +15,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        UserDefaults.standard.register(defaults: [.presentScrollable: true, .rowCount: 20])
+        UserDefaults
+            .standard
+            .register(
+                defaults:
+                    [
+                        .presentScrollable: true,
+                        .rowCount: 20,
+                        .use3DTransforms: {
+                            
+                            if #available(iOS 14, *) { return true }
+                            
+                            return false
+                        }()
+                    ]
+            )
         
         return true
     }
